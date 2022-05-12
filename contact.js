45f031f910c07611f0404df77b75408dc6779a90
v3ry <v3ryfr@gmail.com>
2022-05-12 22:02:31 +0200

    correction et gestion des erreurs sur contact js

--- contact.js
+++ contact.js
@@ -3,14 +3,26 @@ const form = document.getElementById("contact-form");
 const formEvent = form.addEventListener("submit", (event) => {
   event.preventDefault();
   let mail = new FormData(form);
+  
   sendMail(mail);
+
+
 });
 
 const sendMail = (mail) => {
-  fetch("/send", {
+  fetch("http://82.65.82.1:5000/send", {
     method: "post",
     body: mail,
   }).then((response) => {
-    return response.json();
+    console.log(response);
+    if(response.status === 200){
+      console.log("succes")
+    }else{
+      console.log("fail coté serveur");
+    }
+    return response.status;
+  })
+  .catch((error) => {
+    console.log("Serveur HS")
   });
 };

5a07e12f7abe0fda0f6556647a7d6bd360ede975
v3ry <v3ryfr@gmail>
2022-05-13 11:21:48 +0200

    Correction JS pour les recette, générateur boolean

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const recettePreview = document.querySelectorAll(".recettePreview");
 // const recetteFull = document.querySelectorAll(".recetteFull");
-console.log(recettePreview)
+console.log(recettePreview);
 let bAppear = false;
 // for(i = 0; i < recettePreview.length;i++){
 // recettePreview[i].addEventListener("click", fullRecette(i));
 // recetteFull[i].addEventListener("click", fullRecette(i));
 // }
+let myId = 0;
 recettePreview.forEach(rec=>{
+    
+    console.log(myId);
+    rec.setAttribute("ID",myId )
+    window["bAppear"+myId] = false;
+    myId++;
     rec.addEventListener("click",function handleClick(event) {
-        console.log(rec);
-        if (bAppear === false){
+        let currentID = rec.getAttribute("ID");
+        console.log("Id is " + currentID);
+        if (window["bAppear"+currentID] === false){
             rec.classList.add("displayRecette");
             let test = rec.lastElementChild;
             console.log(test);
             rec.lastElementChild.classList.remove("recetteFull")
             console.log(rec)
-            bAppear = true;
+            window["bAppear"+currentID] = true;
         }else{
             rec.classList.remove("displayRecette");
             rec.lastElementChild.classList.add("recetteFull")
-            bAppear = false;
+            window["bAppear"+currentID] = false;
 }})});
 
 
